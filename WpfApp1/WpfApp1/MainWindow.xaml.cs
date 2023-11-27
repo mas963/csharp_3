7369a4573d8a67dcc55b627a851c92d0c86af13b
Yasar Can <38841941+mas963@users.noreply.github.com>
2023-11-28 00:01:04 +0300

    wpf | codewars

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello Guys!");
+        }
+    }
+}
